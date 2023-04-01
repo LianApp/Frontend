988b2b01237351d64cb32d160d6b7007e212c4b7
featt <kachalo55@gmail.com>
2023-04-01 16:51:34 +0300

    add lesson page route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import LessonsPage from '../pages/LessonsPage';
+import LessonPage from '../pages/LessonPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/lessons" element={<LessonsPage />} />
+      <Route path="/lessons/1/pres" element={<LessonPage />} />
     </Routes>
   );
 }
