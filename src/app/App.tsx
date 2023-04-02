17129ebbf70c6b518e57f6202694ea7921b2b5bc
featt <kachalo55@gmail.com>
2023-04-02 19:01:38 +0300

    add lecture page && route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
-import Lesson from '../pages/lesson/Presentation';
+import Presentation from '../pages/lesson/Presentation';
 import Lessons from '../pages/Lessons';
+import Lecture from '../pages/lesson/Lecture';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/lessons" element={<Lessons />} />
-      <Route path="/lessons/1/pres" element={<Lesson />} />
+      <Route path="/lessons/1/pres" element={<Presentation />} />
+      <Route path="/lessons/1/lecture" element={<Lecture />} />
     </Routes>
   );
 }
