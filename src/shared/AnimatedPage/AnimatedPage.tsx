7334c855db109d3faaa5b3ab3eca8731f0a6b70f
featt <kachalo55@gmail.com>
2023-04-02 20:45:29 +0300

    add animation wrapper component

---
+++ src/shared/AnimatedPage/AnimatedPage.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { motion } from 'framer-motion';
+
+interface AnimatedPageProps {
+    children: React.ReactNode
+}
+
+const animations = {
+  initial: {
+    opacity: 0,
+    x: 100,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+  },
+  exit: {
+    opacity: 0,
+    x: -100,
+  },
+};
+
+function AnimatedPage({ children }: AnimatedPageProps) {
+  return (
+    <motion.div variants={animations} initial="initial" animate="animate" exit="exit" transition={{ duration: 0.4 }}>
+      {children}
+    </motion.div>
+  );
+}
+
+export default AnimatedPage;
