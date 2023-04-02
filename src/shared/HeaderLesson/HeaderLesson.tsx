c1828819fac07961c71a79238b8703c429581548
featt <kachalo55@gmail.com>
2023-04-02 18:36:04 +0300

    add links

--- src/shared/HeaderLesson/HeaderLesson.tsx
+++ src/shared/HeaderLesson/HeaderLesson.tsx
@@ -1,5 +1,6 @@
 import { Stack, Typography, Breadcrumbs } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface HeaderLessonProps {
     title: string
@@ -14,9 +15,9 @@ function HeaderLesson({ title }: HeaderLessonProps) {
         {title}
       </Typography>
       <Breadcrumbs>
-        <Typography fontSize="22px" color="text.primary">Презентация</Typography>
-        <Typography fontSize="22px" color="text.primary">Лекция</Typography>
-        <Typography fontSize="22px" color="text.primary">Тест</Typography>
+        <Link style={{ fontSize: '22px', color: 'black' }} to="/lessons/1/pres">Презентация</Link>
+        <Link style={{ fontSize: '22px', color: 'black' }} to="/lessons/1/lecture">Лекция</Link>
+        <Link style={{ fontSize: '22px', color: 'black' }} to="/lessons/1/test">Тест</Link>
       </Breadcrumbs>
     </Stack>
   );
