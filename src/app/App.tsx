d6ae98810fad5ae80c90abbf177cc8f4d7e1b89f
featt <kachalo55@gmail.com>
2023-04-02 20:46:02 +0300

    add animatePresence component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Main from '../pages/Main';
 import Presentation from '../pages/lesson/Presentation';
 import Lessons from '../pages/Lessons';
 import Lecture from '../pages/lesson/Lecture';
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/lessons" element={<Lessons />} />
-      <Route path="/lessons/1/pres" element={<Presentation />} />
-      <Route path="/lessons/1/lecture" element={<Lecture />} />
-    </Routes>
+    <AnimatePresence>
+      <Routes key={location.pathname} location={location}>
+        <Route path="/" element={<Main />} />
+        <Route path="/lessons" element={<Lessons />} />
+        <Route path="/lessons/1/pres" element={<Presentation />} />
+        <Route path="/lessons/1/lecture" element={<Lecture />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
