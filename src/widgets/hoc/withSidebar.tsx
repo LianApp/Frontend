6ad3b47ecf8ef24f308f0d242d5084ec1136ff79
featt <kachalo55@gmail.com>
2023-04-28 23:56:58 +0300

    fix: responsive

--- src/widgets/hoc/withSidebar.tsx
+++ src/widgets/hoc/withSidebar.tsx
@@ -6,11 +6,11 @@ function withSidebar(Component: React.ComponentType) {
   function WithSidebarComponent() {
     return (
       <Grid container maxHeight="100vh" width="100vw" height="100vh">
-        <Grid item lg={2} md={4} xs={6}>
+        <Grid item lg={2} md={2} xs={0}>
           <Sidebar />
         </Grid>
 
-        <Grid item lg={10} md={8} xs={8}>
+        <Grid item lg={10} md={6} xs={12}>
           <Component />
         </Grid>
       </Grid>
