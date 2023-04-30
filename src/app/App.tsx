a2e22098ab76214452628978f8bcfd4a972ea536
featt <kachalo55@gmail.com>
2023-04-30 19:37:42 +0300

    remove comment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
