38f0820162801abd400754dd002f0bb176f689be
featt <kachalo55@gmail.com>
2023-05-01 20:02:02 +0300

    add role state

--- src/entities/user/api/lib/useAuth.ts
+++ src/entities/user/api/lib/useAuth.ts
@@ -1,15 +1,20 @@
+import { Role } from 'entities/user/model/user.model';
 import { create } from 'zustand';
 
 interface Auth {
     userName: string
     accesToken: string
     refreshToken: string
+    role: Role | undefined,
+    setUserRole: (role: Role) => void
 }
 
 const useAuth = create<Auth>()((set) => ({
   accesToken: '',
   refreshToken: '',
   userName: 'User User',
+  role: undefined,
+  setUserRole: (role: Role) => set(() => ({ role: role })),
   setAuthToken: (token: string) => set(() => ({ accesToken: token })),
 }));
 
