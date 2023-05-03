2dae36b04d06374d2dfa468fbdc15487072f269d
featt <kachalo55@gmail.com>
2023-05-04 00:09:09 +0300

    add other fields

--- src/entities/user/api/lib/useAuth.ts
+++ src/entities/user/api/lib/useAuth.ts
@@ -1,21 +1,34 @@
 import { Role } from 'entities/user/model/user.model';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware'
 
 interface Auth {
+    groupId: number
     userName: string
     accesToken: string
     refreshToken: string
-    role: Role | undefined,
+    role: Role | undefined
     setUserRole: (role: Role) => void
+    setAuthToken: (token: string) => void
+    setUserName: (name: string) => void
+    setGroupId: (id: number) => void
 }
 
-const useAuth = create<Auth>()((set) => ({
+//eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+const useAuth = create<Auth>(persist((set) => ({
   accesToken: '',
   refreshToken: '',
   userName: 'User User',
-  role: undefined,
+  role: undefined,  
+  groupId: 2,
   setUserRole: (role: Role) => set(() => ({ role: role })),
   setAuthToken: (token: string) => set(() => ({ accesToken: token })),
+  setUserName: (name: string) => set(() => ({ userName: name })),
+  setGroupId: (id: number) => set(() => ({ groupId: id }))
+}), {
+    name: 'user',
+    version: 1
 }));
 
 export default useAuth;
