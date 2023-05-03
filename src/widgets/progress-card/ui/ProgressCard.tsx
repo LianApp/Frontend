23f90421ba48f9bc37495931cb3882006d8490f0
featt <kachalo55@gmail.com>
2023-05-03 17:07:27 +0300

    add adaptive

--- src/widgets/progress-card/ui/ProgressCard.tsx
+++ src/widgets/progress-card/ui/ProgressCard.tsx
@@ -1,16 +1,24 @@
-import { Box, Typography, CircularProgress } from '@mui/material';
 import React from 'react';
+import { Box, Typography, CircularProgress, useMediaQuery, useTheme } from '@mui/material';
 
 interface LessonCardProps {
-    title: string,
-    progress: number
+  title: string;
+  progress: number;
 }
 
 export function ProgressCard({ title, progress }: LessonCardProps) {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const boxWidth = isSmallScreen ? '100%' : '240px';
+  const boxHeight = isSmallScreen ? 'auto' : '260px';
+  const fontSize = isSmallScreen ? '16px' : '20px';
+  const progressSize = isSmallScreen ? 80 : 120;
+
   return (
     <Box
-      width="240px"
-      height="260px"
+      width={boxWidth}
+      height={boxHeight}
       sx={{
         boxShadow: '0px 4px 4px 4px rgba(74, 0, 233, 0.25)',
         borderRadius: '20px',
@@ -22,8 +30,13 @@ export function ProgressCard({ title, progress }: LessonCardProps) {
       justifyContent="space-around"
       alignItems="center"
     >
-      <Typography fontSize="20px">{title}</Typography>
-      <CircularProgress size={120} sx={{ color: '#9D70FF' }} variant="determinate" value={progress} />
+      <Typography fontSize={fontSize}>{title}</Typography>
+      <CircularProgress
+        size={progressSize}
+        sx={{ color: '#9D70FF' }}
+        variant="determinate"
+        value={progress}
+      />
     </Box>
   );
 }
