c43ec4fb9ab7dc3e424d46bfce0d3672707277b3
featt <kachalo55@gmail.com>
2023-05-04 00:01:31 +0300

    add react-query client

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
