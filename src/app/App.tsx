6e762fa1dd59f67e60a4024318ec1074d04f1b2f
featt <kachalo55@gmail.com>
2023-05-04 18:43:37 +0300

    add routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,7 +9,9 @@ import Test from 'pages/lesson/Test';
 import Login from 'features/authentication/Login/ui/Login';
 import ProtectedRoute from 'features/authentication/ProtectedRoute/ProtectedRoute';
 import { Role } from 'entities/user/model/user.model';
-import TeacherPage from 'pages/TeacherPage';
+import TeacherPage from 'pages/Teacher/TeacherPage';
+import AddLessonPage from 'pages/Teacher/AddLessonPage';
+import LessonsForCourse from 'pages/Teacher/LessonsForCourse';
 
 function App() {
   const location = useLocation();
@@ -17,14 +19,18 @@ function App() {
     <AnimatePresence>
       <Routes key={location.pathname} location={location}>
         <Route element={<ProtectedRoute allowedRoles={[Role.STUDENT]} />}>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Main />} />          
           <Route path="/lessons" element={<Lessons />} />
+        </Route>
+        <Route element={<ProtectedRoute allowedRoles={[Role.STUDENT, Role.TEACHER]}/> } >
           <Route path="/lessons/1/pres" element={<Presentation />} />
           <Route path="/lessons/1/lecture" element={<Lecture />} />
           <Route path="/lessons/1/test" element={<Test />} />
         </Route>
         <Route element={<ProtectedRoute allowedRoles={[Role.TEACHER]}/>}>
           <Route path='/teacher' element={<TeacherPage />} />
+          <Route path='/teacher/course/lessons' element={<LessonsForCourse />}/>
+          <Route path='/add-lesson' element={<AddLessonPage />}/>
         </Route>
         <Route path="/login" element={<Login />}/>
       </Routes>
